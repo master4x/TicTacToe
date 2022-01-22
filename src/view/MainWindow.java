216e7ae6d09f3676b3d22c2c2cdfa4f3e2df6cfe
Leon Niklas Kelle <42148131+master4x@users.noreply.github.com>
2022-01-22 16:27:25 +0100

    Finished Basic Design

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -13,6 +13,13 @@ import java.awt.Toolkit;
 import java.awt.Dimension;
 import javax.swing.ImageIcon;
 import javax.swing.border.BevelBorder;
+import javax.swing.SwingConstants;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import javax.swing.JSeparator;
 
 /**
  * @author Leon Kelle
@@ -26,6 +33,11 @@ public class MainWindow
 	public JFrame frmTicTacToe;
 	private JTextField txtOpponentsIp;
 	private JTextField txtYourIp;
+	private JTable tblGameStats;
+	private JTextField txtWinCount;
+	private JTextField txtLooseCount;
+	private JTextField txtDrawCount;
+	private JLabel lblDrawCount;
 
 	/**
 	 * Create the application.
@@ -107,19 +119,111 @@ public class MainWindow
 		pnlTabGame.add(pnlMiddleRow, BorderLayout.CENTER);
 		pnlMiddleRow.setLayout(new BorderLayout(0, 0));
 		
-		JPanel pnlRightOuterContainer = new JPanel();
-		pnlRightOuterContainer.setBorder(new EmptyBorder(0, 5, 0, 5));
-		pnlMiddleRow.add(pnlRightOuterContainer, BorderLayout.EAST);
-		pnlRightOuterContainer.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblCallPlayerAction = new JLabel("Your Turn!");
-		pnlRightOuterContainer.add(lblCallPlayerAction, BorderLayout.CENTER);
-		
 		JPanel pnlCenterContainer = new JPanel();
+		pnlCenterContainer.setBorder(new EmptyBorder(25, 25, 25, 25));
 		pnlMiddleRow.add(pnlCenterContainer, BorderLayout.CENTER);
+		pnlCenterContainer.setLayout(new BorderLayout(0, 0));
+		
+		JPanel pnlGameField = new JPanel();
+		pnlGameField.setBorder(new EmptyBorder(0, 25, 0, 25));
+		pnlCenterContainer.add(pnlGameField, BorderLayout.WEST);
+		pnlGameField.setLayout(new GridLayout(3, 3, 50, 0));
+		
+		JLabel lblGameField00 = new JLabel("0");
+		lblGameField00.setHorizontalAlignment(SwingConstants.CENTER);
+		pnlGameField.add(lblGameField00);
+		
+		JLabel lblGameField01 = new JLabel("0");
+		pnlGameField.add(lblGameField01);
+		
+		JLabel lblGameField02 = new JLabel("0");
+		lblGameField02.setHorizontalAlignment(SwingConstants.CENTER);
+		pnlGameField.add(lblGameField02);
+		
+		JLabel lblGameField10 = new JLabel("0");
+		lblGameField10.setHorizontalAlignment(SwingConstants.CENTER);
+		pnlGameField.add(lblGameField10);
+		
+		JLabel lblGameField11 = new JLabel("0");
+		lblGameField11.setHorizontalAlignment(SwingConstants.CENTER);
+		pnlGameField.add(lblGameField11);
+		
+		JLabel lblGameField12 = new JLabel("0");
+		lblGameField12.setHorizontalAlignment(SwingConstants.CENTER);
+		pnlGameField.add(lblGameField12);
+		
+		JLabel lblGameField20 = new JLabel("0");
+		lblGameField20.setHorizontalAlignment(SwingConstants.CENTER);
+		pnlGameField.add(lblGameField20);
+		
+		JLabel lblGameField21 = new JLabel("0");
+		lblGameField21.setHorizontalAlignment(SwingConstants.CENTER);
+		pnlGameField.add(lblGameField21);
+		
+		JLabel lblGameField22 = new JLabel("0");
+		lblGameField22.setHorizontalAlignment(SwingConstants.CENTER);
+		pnlGameField.add(lblGameField22);
+		
+		JPanel pnlGameInfo = new JPanel();
+		pnlCenterContainer.add(pnlGameInfo, BorderLayout.EAST);
+		pnlGameInfo.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblYourTurn = new JLabel("Your Turn!");
+		lblYourTurn.setHorizontalAlignment(SwingConstants.CENTER);
+		pnlGameInfo.add(lblYourTurn);
 		
 		JPanel pnlTabStats = new JPanel();
 		pnlTabStats.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		pneNavBar.addTab("Stats", new ImageIcon(MainWindow.class.getResource("/org/eclipse/jface/fieldassist/images/info_ovr@2x.png")), pnlTabStats, null);
+		pnlTabStats.setLayout(new BorderLayout(0, 0));
+		
+		JPanel pnlTopMain = new JPanel();
+		pnlTabStats.add(pnlTopMain, BorderLayout.NORTH);
+		pnlTopMain.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblWinCount = new JLabel("Win Count:");
+		pnlTopMain.add(lblWinCount);
+		
+		txtWinCount = new JTextField();
+		txtWinCount.setEditable(false);
+		pnlTopMain.add(txtWinCount);
+		txtWinCount.setColumns(10);
+		
+		JLabel lblLooseCount = new JLabel("Loose Count:");
+		pnlTopMain.add(lblLooseCount);
+		
+		txtLooseCount = new JTextField();
+		txtLooseCount.setEditable(false);
+		pnlTopMain.add(txtLooseCount);
+		txtLooseCount.setColumns(10);
+		
+		lblDrawCount = new JLabel("Draw Count:");
+		pnlTopMain.add(lblDrawCount);
+		
+		txtDrawCount = new JTextField();
+		txtDrawCount.setEditable(false);
+		pnlTopMain.add(txtDrawCount);
+		txtDrawCount.setColumns(10);
+		
+		JPanel pnlCenterMain = new JPanel();
+		pnlTabStats.add(pnlCenterMain);
+		pnlCenterMain.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrlStatsTable = new JScrollPane();
+		scrlStatsTable.setViewportBorder(null);
+		pnlCenterMain.add(scrlStatsTable);
+		
+		tblGameStats = new JTable();
+		tblGameStats.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column", "New column"
+			}
+		));
+		scrlStatsTable.setViewportView(tblGameStats);
 	}
 }
