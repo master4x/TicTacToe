fec6fb565364dfa8d5545792d05114e55cf33b05
Leon Niklas Kelle <42148131+master4x@users.noreply.github.com>
2022-01-22 15:13:55 +0100

    Add switch case in onGameStateChange

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -9,10 +9,40 @@ package logic;
 public class Game
 {
 	private int[][] gameField = new int[3][3];
-	private GameState gameState;
+	private GameState gameState = GameState.NoGameRunning;
 	
 	public void onGameStateChange(GameState gameState)
 	{
-		this.gameState = gameState;
+		if (this.gameState != gameState)
+		{
+			this.gameState = gameState;
+			
+			switch (gameState)
+			{
+			case CheckOpponentsGameField:
+				break;
+			case CheckPlayersGameField:
+				break;
+			case ConnectingWithOpponent:
+				break;
+			case GameOver_Draw:
+				break;
+			case GameOver_Loose:
+				break;
+			case GameOver_Win:
+				break;
+			case NoGameRunning:
+				break;
+			case SendPlayersGameField:
+				break;
+			case WaitForOpponentsGameField:
+				break;
+			case WaitForPlayersGameField:
+				break;
+			default:
+				break;
+			
+			}
+		}
 	}
 }
