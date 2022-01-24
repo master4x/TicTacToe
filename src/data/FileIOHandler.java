88c9052d9170fa4611f875d8a53a9edca4a4cfe1
Leon Niklas Kelle <42148131+master4x@users.noreply.github.com>
2022-01-24 17:40:13 +0100

    Insert FileIOExample

--- src/data/FileIOHandler.java
+++ src/data/FileIOHandler.java
@@ -1,5 +1,12 @@
 package data;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * @author Leon Kelle
  * @class AGB\AH911
@@ -8,5 +15,42 @@ package data;
 
 public class FileIOHandler
 {
+	public ArrayList<String> readFile(String pathAndFileName)
+	{
+		ArrayList<String> fileAsList = new ArrayList<>();
 
+		try (FileReader fileReader = new FileReader(pathAndFileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);)
+		{
+			String line = bufferedReader.readLine(); // Read first Line
+
+			while (line != null)
+			{
+				fileAsList.add(line);
+				line = bufferedReader.readLine();
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+		return fileAsList;
+	}
+
+	public void writeFile(String pathAndFileName)
+	{
+		try (FileWriter fileWriter = new FileWriter(pathAndFileName);
+			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter))
+		{
+			bufferedWriter.write("Erste Zeile");
+			bufferedWriter.newLine(); // LineBreak
+			bufferedWriter.write("Zweite Zeile, die ein wenig länger ist.");
+			bufferedWriter.flush();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
