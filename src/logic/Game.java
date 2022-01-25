4c8336056bcd0a24fe06984c34a1a2f81d60d3d3
Leon Niklas Kelle <42148131+master4x@users.noreply.github.com>
2022-01-25 21:19:02 +0100

    Reimplemented messsy check function

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -13,16 +13,16 @@ public class Game
 	private int[][] gameField = new int[3][3];
 	private GameState gameState = GameState.NoGameRunning;
 	private Players player;
-	
+
 	public void startGame(String ipAdress)
 	{
 		NetworkHandler.getInstance().newNetworkSocket(ipAdress);
 	}
-	
+
 	@SuppressWarnings("incomplete-switch")
 	private void gameOver()
 	{
-		//TODO print GameOver
+		// TODO print GameOver
 		switch (gameState)
 		{
 			case GameOver_Draw:
@@ -32,83 +32,69 @@ public class Game
 			case GameOver_Win:
 				break;
 		}
-		
-		//TODO save CSV result
-		
+
+		// TODO save CSV result
+
 		setGameState(GameState.NoGameRunning);
 	}
-	
+
 	private synchronized void checkPlayerMove()
 	{
-		//TODO check Input and change gameField
+		// TODO check Input and change gameField
 	}
 
 	private void checkGameField()
 	{
-		boolean player1Wins = false, player2Wins = false;
-
-		for (int i = 1; i < 3; i++)
+		if (gameField[0][0] != 0 && gameField[0][1] != 0 && gameField[0][2] != 0 ||
+			gameField[1][0] != 0 && gameField[1][1] != 0 && gameField[1][2] != 0 ||
+			gameField[2][0] != 0 && gameField[2][1] != 0 && gameField[2][2] != 0 ||
+			gameField[0][0] != 0 && gameField[1][0] != 0 && gameField[2][2] != 0 ||
+			gameField[0][1] != 0 && gameField[1][1] != 0 && gameField[1][1] != 0 ||
+			gameField[0][2] != 0 && gameField[1][2] != 0 && gameField[1][2] != 0 ||
+			gameField[0][1] != 0 && gameField[1][1] != 0 && gameField[2][2] != 0 ||
+			gameField[0][2] != 0 && gameField[1][1] != 0 && gameField[2][0] != 0)
 		{
-			if (gameField[0][0] == i && gameField[0][1] == i && gameField[0][2] == i || // top row
-				gameField[1][0] == i && gameField[1][1] == i && gameField[1][2] == i || // middle row
-				gameField[2][0] == i && gameField[2][1] == i && gameField[2][2] == i || // bottom row
-				gameField[0][0] == i && gameField[1][0] == i && gameField[2][2] == i || // left column
-				gameField[0][1] == i && gameField[1][1] == i && gameField[1][1] == i || // middle column
-				gameField[0][2] == i && gameField[1][2] == i && gameField[1][2] == i || // right column
-				gameField[0][1] == i && gameField[1][1] == i && gameField[2][2] == i || // TL to BR diagonal
-				gameField[0][2] == i && gameField[1][1] == i && gameField[2][0] == i) // TR to BL diagonal
-			{
-				switch (i)
-				{
-					case 1:
-						player1Wins = true;
-						break;
-					case 2:
-						player2Wins = true;
-						break;
-				}
-			}
+			setGameState(GameState.GameOver_Draw);
 		}
-
-		if (player1Wins && player2Wins)
-		{
-			setGameState(GameState.GameOver_Draw); //TODO wrong
-		}
-		else if (player1Wins || player2Wins)
+		else if (gameField[0][0] == 1 && gameField[0][1] == 1 && gameField[0][2] == 1 ||
+			gameField[1][0] == 1 && gameField[1][1] == 1 && gameField[1][2] == 1 ||
+			gameField[2][0] == 1 && gameField[2][1] == 1 && gameField[2][2] == 1 ||
+			gameField[0][0] == 1 && gameField[1][0] == 1 && gameField[2][2] == 1 ||
+			gameField[0][1] == 1 && gameField[1][1] == 1 && gameField[1][1] == 1 ||
+			gameField[0][2] == 1 && gameField[1][2] == 1 && gameField[1][2] == 1 ||
+			gameField[0][1] == 1 && gameField[1][1] == 1 && gameField[2][2] == 1 ||
+			gameField[0][2] == 1 && gameField[1][1] == 1 && gameField[2][0] == 1)
 		{
 			if (player == Players.Player1)
 			{
-				if (player1Wins)
-				{
-					setGameState(GameState.GameOver_Win);
-				}
-				else
-				{
-					setGameState(GameState.GameOver_Loose);
-				}
+				setGameState(GameState.GameOver_Win);
 			}
-			else if (player == Players.Player2)
+			else
 			{
-				if (player2Wins)
-				{
-					setGameState(GameState.GameOver_Win);
-				}
-				else
-				{
-					setGameState(GameState.GameOver_Loose);
-				}
+				setGameState(GameState.GameOver_Loose);
+			}
+		}
+		else if (gameField[0][0] == 2 && gameField[0][1] == 2 && gameField[0][2] == 2 ||
+			gameField[1][0] == 2 && gameField[1][1] == 2 && gameField[1][2] == 2 ||
+			gameField[2][0] == 2 && gameField[2][1] == 2 && gameField[2][2] == 2 ||
+			gameField[0][0] == 2 && gameField[1][0] == 2 && gameField[2][2] == 2 ||
+			gameField[0][1] == 2 && gameField[1][1] == 2 && gameField[1][1] == 2 ||
+			gameField[0][2] == 2 && gameField[1][2] == 2 && gameField[1][2] == 2 ||
+			gameField[0][1] == 2 && gameField[1][1] == 2 && gameField[2][2] == 2 ||
+			gameField[0][2] == 2 && gameField[1][1] == 2 && gameField[2][0] == 2)
+		{
+			if (player == Players.Player2)
+			{
+				setGameState(GameState.GameOver_Win);
+			}
+			else
+			{
+				setGameState(GameState.GameOver_Loose);
 			}
 		}
 		else
 		{
-			if (gameState == GameState.CheckOpponentsGameField)
-			{
-				setGameState(GameState.WaitForPlayersGameField);
-			}
-			else if (gameState == GameState.CheckPlayersGameField)
-			{
-				setGameState(GameState.SendPlayersGameField);
-			}
+			setGameState(GameState.WaitForOpponentsGameField);
 		}
 	}
 
