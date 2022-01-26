3d1cdc2b9124b8daca77a21958b6f77b7bc4b1b2
Leon Niklas Kelle <42148131+master4x@users.noreply.github.com>
2022-01-26 11:47:56 +0100

    Deleted network test section

--- src/controller/Startup.java
+++ src/controller/Startup.java
@@ -30,11 +30,5 @@ public class Startup
 				}
 			}
 		});
-
-		// TEST networking
-		int[][] testArr = { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
-		NetworkHandler.getInstance().newNetworkSocket("127.0.0.1");
-		NetworkHandler.getInstance().sendArray(testArr);
-		NetworkHandler.getInstance().receiveArray();
 	}
 }
